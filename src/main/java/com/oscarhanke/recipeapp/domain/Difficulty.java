59186f9cb52aa7abdfb31aabcc59c128cd514f9c
ohanke <steamoha@wp.pl>
2022-05-03 12:40:55 +0200

    Added Difficuly Enum

---
+++ src/main/java/com/oscarhanke/recipeapp/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.oscarhanke.recipeapp.domain;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
--- src/main/java/com/oscarhanke/recipeapp/domain/Recipe.java
+++ src/main/java/com/oscarhanke/recipeapp/domain/Recipe.java
@@ -16,13 +16,14 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe") //property    from the child class
     private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -97,5 +98,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
