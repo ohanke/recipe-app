783db9686d627d63e9e29942a32c4bf3d8ce2b88
ohanke <steamoha@wp.pl>
2022-05-03 12:31:17 +0200

    Completed Assignment.

--- src/main/java/com/oscarhanke/recipeapp/domain/Ingredient.java
+++ src/main/java/com/oscarhanke/recipeapp/domain/Ingredient.java
@@ -10,7 +10,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -37,6 +39,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
