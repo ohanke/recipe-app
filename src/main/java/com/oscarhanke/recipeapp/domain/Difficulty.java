67792d9878a8c2a237b8b6ae46ee6276ff7dd55e
ohanke <steamoha@wp.pl>
2022-05-07 12:19:01 +0200

    Fixes bugs

--- src/main/java/com/oscarhanke/recipeapp/domain/Difficulty.java
+++ src/main/java/com/oscarhanke/recipeapp/domain/Difficulty.java
@@ -1,7 +1,9 @@
 package com.oscarhanke.recipeapp.domain;
 
+/**
+ * Created by jt on 6/13/17.
+ */
 public enum Difficulty {
-    EASY,
-    MODERATE,
-    HARD
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
 }
--- src/main/java/com/oscarhanke/recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/oscarhanke/recipeapp/domain/UnitOfMeasure.java
@@ -1,9 +1,16 @@
 package com.oscarhanke.recipeapp.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**
+ * Created by jt on 6/13/17.
+ */
 @Entity
 public class UnitOfMeasure {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
