f003f1c3595495595d97ae82f950f4c18e702686
ohanke <steamoha@wp.pl>
2022-05-12 13:55:31 +0200

    Spring Integration Tests

---
+++ src/test/java/com/oscarhanke/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,55 @@
+package com.oscarhanke.recipeapp.controllers;
+
+import com.oscarhanke.recipeapp.Services.RecipeService;
+import com.oscarhanke.recipeapp.domain.Recipe;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class IndexControllerTest {
+
+   @Mock RecipeService recipeService;
+   @Mock Model model;
+   IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+        //then
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
+}
