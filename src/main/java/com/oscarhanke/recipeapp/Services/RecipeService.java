12c88049b7817515e45a240a97443c458b588d54
ohanke <steamoha@wp.pl>
2022-05-18 17:21:06 +0200

    Modified services

--- src/main/java/com/oscarhanke/recipeapp/Services/RecipeService.java
+++ src/main/java/com/oscarhanke/recipeapp/Services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.oscarhanke.recipeapp.Services;
 
+import com.oscarhanke.recipeapp.commands.RecipeCommand;
 import com.oscarhanke.recipeapp.domain.Recipe;
 
 import java.util.Set;
@@ -8,4 +9,6 @@ public interface RecipeService{
     Set<Recipe> getRecipes();
 
     Recipe findById(Long id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
--- src/main/java/com/oscarhanke/recipeapp/Services/RecipeServiceImpl.java
+++ src/main/java/com/oscarhanke/recipeapp/Services/RecipeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.oscarhanke.recipeapp.Services;
 
+import com.oscarhanke.recipeapp.commands.RecipeCommand;
+import com.oscarhanke.recipeapp.converters.RecipeCommandToRecipe;
+import com.oscarhanke.recipeapp.converters.RecipeToRecipeCommand;
 import com.oscarhanke.recipeapp.domain.Recipe;
 import com.oscarhanke.recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -14,27 +18,43 @@ import java.util.Set;
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
     public Set<Recipe> getRecipes() {
-        log.debug("i'm in the service");
-        Set<Recipe> recipes = new HashSet<>();
-        recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
-        return recipes;
+        log.debug("I'm in the service");
+
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
     }
 
     @Override
-    public Recipe findById(Long id){
-        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+    public Recipe findById(Long l) {
 
-        if (!recipeOptional.isPresent()){
-            throw new RuntimeException("Recipe not found!");
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe Not Found!");
         }
 
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
